83fa97801ad84b5cc5b909f512097793cf52df1c
Etienne Binginot <etienne.binginot@gmail.com>
2025-10-19 12:42:39 +0200

    feat: initial commit

---
+++ src/main/java/fr/physigames/PhysiGamesApplication.java
@@ -0,0 +1,13 @@
+package fr.physigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/physigames/PhysiGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.physigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
